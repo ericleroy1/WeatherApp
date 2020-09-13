059ffecea97af5580cb28a4bb46301e6353fb384
ericleroy1 <54584831+ericleroy1@users.noreply.github.com>
2020-09-13 19:37:25 +1000

    functioning form and updating display

--- app.js
+++ app.js
@@ -5,17 +5,26 @@ const baseURL = "http://api.openweathermap.org/data/2.5/weather?q=";
 const searchbox = document.getElementById("city");
 const submitButton = document.getElementById("submitButton")
 
+var form = document.getElementById('mainForm')
 submitButton.addEventListener('click', setQuery);
 
 function setQuery(e){
   city = searchbox.value;
-  console.log(city);
   getData();
   e.preventDefault();
+  mainForm.reset();
 }
 
 function getData(){
-  fetch(baseURL + city + "&appid=" + apiKey)
-  .then(response => response.json())
-  .then(data => console.log(data));
+  fetch(baseURL + city + "&units=metric&appid=" + apiKey)
+  .then(weather => {
+    return weather.json();
+  }).then(displayResults);
+}
+
+function displayResults(weather){
+  document.getElementById("cityDiv").innerText = weather.name;
+  document.getElementById("tempDiv").innerText = Math.round(weather.main.temp) + "°c";
+  document.getElementById("humidityDiv").innerText = weather.main.humidity + "% humidity";
+  document.getElementById("conditionDiv").innerText = weather.weather[0].description;
 }
