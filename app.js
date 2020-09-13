e0ef6591417ce83ac904f0741ff90d51fd48df30
ericleroy1 <54584831+ericleroy1@users.noreply.github.com>
2020-09-13 18:58:11 +1000

    functioning API call

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
 
 const apiKey = "0f980128ca62c145b25f5e373a1f3b37";
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?q=";
 
+const searchbox = document.getElementById("city");
+const submitButton = document.getElementById("submitButton")
 
-// document.getElementById('inputCity').addEventListener("submit", function(){
-// //
-//   let city = document.getElementById('city').value;
-//   let url = "http://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + city + "&units=metric"
-// //
-//   console.log(city);
-//   console.log(apiKey)
-//   console.log(url);
-//
-// });
+submitButton.addEventListener('click', setQuery);
 
+function setQuery(e){
+  city = searchbox.value;
+  console.log(city);
+  getData();
+  e.preventDefault();
+}
 
-
-function submitCity(){
-
-  console.log("submitted")
-  city = document.getElementById('city').value;
-  url = "http://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + city + "&units=metric"
-
-  console.log(city, url)
-
-    function getData(){
-      fetch(url)
-      .then(response => response.json())
-      .then(data => console.log(data));
-    }
-
-getData()
-
-  }
+function getData(){
+  fetch(baseURL + city + "&appid=" + apiKey)
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
